44798b9f8e64679bb15965d6521c84ee3350bafa
nvorkapic <nikola.vorkapic@hotmail.com>
2016-05-11 14:41:02 +0200

    x

---
+++ TestInterface/HubInterface.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TestInterface.Models;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestInterface
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HubInterface : Page
+    {
+        public HubInterface()
+        {
+            this.InitializeComponent();
+        }
+
+        private void LoadListView(object sender, RoutedEventArgs e)
+        {
+            var listView = (ListView)sender;
+            
+            listView.ItemsSource = (Application.Current as App).MeasurementConfiguration;
+        }
+
+        private void ListSelectionChange(object sender, SelectionChangedEventArgs e)
+        {
+    
+        }
+    }
+
+
+}
